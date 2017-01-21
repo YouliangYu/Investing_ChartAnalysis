698e60df977ca9a0f59def370551ec6aa27be1fa
youliang <youliang1229@gmail.com>
2017-01-21 00:30:26 -0600

    delete

--- DataExtractor.py
+++
@@ -1,9 +0,0 @@
-from pandas_datareader import data
-from datetime import datetime
-import matplotlib
-import h5py
-
-#nvda = data.DataReader('NVDA', 'yahoo',datetime(2016,11,1),datetime(2016,12,30))
-#print(nvda['Adj Close'].head())
-aapl = data.Options('aapl', 'yahoo')
-data = aapl.get_all_data()
